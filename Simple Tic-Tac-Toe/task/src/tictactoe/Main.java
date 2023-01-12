de9f8d986b10a9fef141d95928c9a0831840bd44
Dragan Cajic <cajic_dragan@yahoo.com>
2023-01-13 00:24:43 +0100

    Implement Stage 2/5: The user is the gamemaster

--- Simple Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Simple Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -1,10 +1,23 @@
 package tictactoe;
 
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
 		// write your code here
-		System.out.println("X O X");
-		System.out.println("O X O");
-		System.out.println("X X O");
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Enter cells: ");
+		String input = scanner.nextLine();
+		
+		System.out.println("---------");
+		for (int i = 0; i < 3; i++) {
+			System.out.print("| ");
+			for (int j = 0; j < 3; j++) {
+				System.out.print(input.charAt(3 * i + j) + " ");
+			}
+			System.out.println("|");
+		}
+		System.out.println("---------");
 	}
 }
